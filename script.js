763b1c7067f8d035678f603624c79cc002f6871f
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2023-10-01 12:56:18 +0100

    Create operate function
    operate() takes in three parameters (an operator and 2 numbers) and then calls the appropriate function on the numbers.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1;
+let operator;
+let num2;
+
 function add(a, b) {
 	return a + b;
 }
@@ -12,4 +16,23 @@ function multiply(a, b) {
 
 function divide(a, b) {
 	return a / b;
+}
+
+function operate(a, operator, b) {
+	switch (operator) {
+		case "+":
+			return add(a, b);
+			break;
+		case "-":
+			return subtract(a, b);
+			break;
+		case "*":
+			return multiply(a, b);
+			break;
+		case "/":
+			return divide(a, b);
+			break;
+		default:
+			return "Invalid operator";
+	}
 }
