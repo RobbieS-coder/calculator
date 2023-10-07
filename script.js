8a2487e2c2cc85988e228513a2960c356a43ba76
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2023-10-07 08:20:30 +0100

    Create calculator design in HTML
    No JavaScript is linked yet so nothing happens when you press the buttons.

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const buttons = document.querySelectorAll("button");
+
+const button1 = document.querySelector(".btn-1");
+const button2 = document.querySelector(".btn-2");
+const button3 = document.querySelector(".btn-3");
+const button4 = document.querySelector(".btn-4");
+const button5 = document.querySelector(".btn-5");
+const button6 = document.querySelector(".btn-6");
+const button7 = document.querySelector(".btn-7");
+const button8 = document.querySelector(".btn-8");
+const button9 = document.querySelector(".btn-9");
+const buttonDecimal = document.querySelector(".btn-decimal");
+const button0 = document.querySelector(".btn-0");
+const buttonEquals = document.querySelector(".btn-equals");
+
+buttons.forEach(button => {
+	button.addEventListener("mouseenter", () => {
+			button.style.backgroundColor = "rgb(185, 180, 182)";
+	});
+
+	button.addEventListener("mouseleave", () => {
+			button.style.backgroundColor = "rgb(213, 197, 200)";
+	});
+
+	button.addEventListener("mousedown", () => {
+		button.style.backgroundColor = "rgb(157, 163, 164)";
+	});
+
+	button.addEventListener("mouseup", () => {
+		button.style.backgroundColor = "rgb(185, 180, 182)";
+	});
+});
+
 let num1;
 let operator;
 let num2;
